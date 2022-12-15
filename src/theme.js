c01f2924a8631dc0d9737af1b6c0313c19b04f43
lucas-abc <lucas.abc@protonmail.com>
2022-12-15 09:42:28 -0300

    add theme

---
+++ src/theme.js
@@ -0,0 +1,102 @@
+export const colorTokens = {
+  grey: {
+    0: "#ffffff",
+    10: "#eaeaea",
+    50: "#d5d5d5",
+    100: "#bfbfbf",
+    200: "#aaaaaa",
+    300: "#959595",
+    400: "#808080",
+    500: "#6a6a6a",
+    600: "#555555",
+    700: "#404040",
+    800: "#2b2b2b",
+    900: "#151515",
+    1000: "#000000",
+  },
+  primary: {
+    50: "#ffffff",
+    100: "#ccf0ff",
+    200: "#99e1ff",
+    300: "#65d2ff",
+    400: "#32c3ff",
+    500: "#32c3ff",
+    600: "#2692bf",
+    700: "#196280",
+    800: "#0d3140",
+    900: "#000000",
+  },
+};
+
+export const themeSettings = (mode) => {
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              dark: colorTokens.primary[200],
+              main: colorTokens.primary[500],
+              light: colorTokens.primary[800],
+            },
+            neutral: {
+              dark: colorTokens.grey[100],
+              main: colorTokens.grey[200],
+              mediumMain: colorTokens.grey[300],
+              medium: colorTokens.grey[400],
+              light: colorTokens.grey[700],
+            },
+            background: {
+              default: colorTokens.grey[900],
+              alt: colorTokens.grey[800],
+            },
+          }
+        : {
+            primary: {
+              dark: colorTokens.primary[700],
+              main: colorTokens.primary[500],
+              light: colorTokens.primary[50],
+            },
+            neutral: {
+              dark: colorTokens.grey[700],
+              main: colorTokens.grey[500],
+              mediumMain: colorTokens.grey[400],
+              medium: colorTokens.grey[300],
+              light: colorTokens.grey[50],
+            },
+            background: {
+              default: colorTokens.grey[10],
+              alt: colorTokens.grey[0],
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Roboto", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
+};
